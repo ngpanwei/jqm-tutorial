8bd9ed8a540c401f6e82b99bb37c67a8b9b6c6a0
ngpanwei <dev@ngpanwei.com>
2015-05-01 21:25:34 +0800

    triangle

--- qunit/triangle/triangle.js
+++ qunit/triangle/triangle.js
@@ -1,10 +1,12 @@
 var triangle = {
     isEquilateral : function(a,b,c) {
-    	    if(a!=b) return false ;
+    	    if(a!=b) return false ; 
     	    if(b!=c) return false ;
+    	    if(a<=0) return false ;
     		return true ;
     },
     isIsosceles : function(a,b,c) {
     	    return true ;
     }
 };
+
